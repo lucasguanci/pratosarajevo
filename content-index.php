389749154f8c8544cb1a360a18af6db5f594cec9
luca sguanci <luca.sguanci@gmail.com>
2014-04-26 10:41:53 +0200

    0.1.3 - minor changes layout index.html

--- content-index.php
+++ content-index.php
@@ -123,62 +123,6 @@ Caterina Fanfani <a href="mailto://ufficiostampa@provincia.prato.it">ufficiostam
         </div>
         <div class="clearfix visible-sm visible-xs"></div>
       </div>
-          <!-- content -->
-          <div class="col-md-9 content">
-          <!-- intro -->
-          <header class="intro">
-            <article>
-              <h2>CAP Autolinee</h2>
-              <p>Lorem ipsum dolor sit amet, <strong>consetetur</strong> sadipscing elitr, sed diam nonumy <em>eirmod tempor</em> invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-            </article>
-          </header>
-          <!-- /intro -->
-          <!-- highlights -->
-          <div class="row highlights">
-            <div class="col-md-6">
-              <article>
-                <figure>
-                  <a href="tpl-notizie-approfondimenti-dettaglio.php"><img src="img/highlights-1.jpg"/></a>
-                  <figcaption>At vero eos</figcaption>
-                </figure>
-                <h2><a href="tpl-notizie-approfondimenti-dettaglio.php">Sale semper intellegat qui an, in mei vero doming constituto</a></h2>
-                <p>Et alii nemore mel, ius at indoctum urbanitas persecuti. Ex eos veniam nemore alienum, eu amet tibique liberavisse sea. Usu at purto mollis temporibus. Has ex falli doctus imperdiet. An illum sadipscing quo, eum debet consequat no. Vix nibh sonet aperiri ne, te mea libris scripserit.</p>
-              </article>
-            </div>
-            <div class="col-md-6">
-              <article>
-                <figure>
-                  <a href="tpl-notizie-approfondimenti-dettaglio.php"><img src="img/highlights-2.jpg"/></a>
-                  <figcaption>At vero eos</figcaption>
-                </figure>
-                <h2><a href="tpl-notizie-approfondimenti-dettaglio.php">Sale semper intellegat qui an, in mei vero doming constituto</a></h2>
-                <p>Et alii nemore mel, ius at indoctum urbanitas persecuti. Ex eos veniam nemore alienum, eu amet tibique liberavisse sea. Usu at purto mollis temporibus. Has ex falli doctus imperdiet. An illum sadipscing quo, eum debet consequat no. Vix nibh sonet aperiri ne, te mea libris scripserit.</p>
-              </article>
-            </div>
-          </div>
-          <!-- /highlights -->
-          <!-- small highlights -->
-          <div class="row small-highlights">
-            <div class="col-md-6">
-              <article>
-                <figure>
-                  <a href="tpl-notizie-approfondimenti-dettaglio.php"><img src="img/small-highlights-1.jpg"/></a>
-                  <figcaption>At vero eos</figcaption>
-                </figure>
-              </article>
-            </div>
-            <div class="col-md-6">
-              <article>
-                <figure>
-                  <a href="tpl-notizie-approfondimenti-dettaglio.php"><img src="img/small-highlights-2.jpg"/></a>
-                  <figcaption>At vero eos</figcaption>
-                </figure>
-              </article>
-            </div>
-          </div>
-          <!-- /small highlights -->
-          </div>
-          <!-- /content -->
     </div>
   </section>
   <!-- /content -->    
